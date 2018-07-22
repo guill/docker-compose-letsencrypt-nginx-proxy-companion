e4dbb5415fd523c54e6b1935baf33664d4eac38e
Jacob Segal <jacob.e.segal@gmail.com>
2018-07-22 13:08:06 -0700

    Don't download config file by default

--- start.sh
+++ start.sh
@@ -23,7 +23,8 @@ if [ ! -z ${SERVICE_NETWORK+X} ]; then
 fi
 
 # 4. Download the latest version of nginx.tmpl
-curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > nginx.tmpl
+# JS - Don't do this. Potential security issue.
+#curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > nginx.tmpl
 
 # 5. Update local images
 docker-compose pull
